35c989c6aa24d3efe8d4a747cc2c67ee58b497e1
EliteDev Emon <elitedevemon@gmail.com>
2024-09-30 04:59:13 +0600

    initial commit

--- resources/views/Tailwindcss/tailwindcss.blade.php
+++ resources/views/Tailwindcss/tailwindcss.blade.php
@@ -4,11 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  @vite('resources/css/app.css')
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <title>Check Tailwindcss</title>
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline text-blue-300 font-mono">
+  <h1 class="text-3xl font-bold underline text-blue-300 font-mono bg-gray-500">
     Hello world!
   </h1>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,231 +1,37 @@
 <?php
 
-use App\Http\Controllers\component\checkComponentController;
-use App\Http\Controllers\FormValidationController;
-use App\Http\Controllers\GroupController;
-use App\Http\Controllers\invokable\events\modelEventControl;
-use App\Http\Controllers\invokable\relationship\belongsToManyController;
-use App\Http\Controllers\invokable\relationship\hasManyController;
-use App\Http\Controllers\invokable\relationship\hasOneController;
-use App\Http\Controllers\invokable\relationship\hasOneThroughController;
-use App\Http\Controllers\invokable\relationship\morphToController;
-use App\Http\Controllers\InvokeController;
-use App\Http\Controllers\MultipleWhereController;
-use App\Http\Controllers\PaginationController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\resource\relationship\morphToResourceController;
-use App\Http\Controllers\TeacherController;
 
 Route::get('/', function () {
-  return view('welcome');
+    return view('welcome');
 });
 
-/**
- * Testing parameter @param id and optional parameter @param id
- * TODO: passing  the "?" sign to make the parameter optional
- */
-Route::get('welcome/{id?}', function ($id = null) {
-  if ($id) {
-    return $id;
-  }
-  return "There have nothing to show. No ID found";
+Route::get('test-schedule', function(){
+  Artisan::call('schedule:run');
+  return 'Schedule work running';
 });
 
-/**
- * Route parameter type selection @param id
- * TODO: use the whereNumber method to select the parameter type to a number
- */
-Route::get('parameter-type-number/{id}', function ($id = null) {
-  return $id;
-})->whereNumber('id');
-
-/** 
- * TODO: use the whereAlpha method to select the parameter type to a alphabet
- */
-Route::get('/parameter-type-alpha/{string}', function (string $string) {
-  return $string;
-})->whereAlpha('string');
-
-/** 
- * TODO: use the whereAlphaNumeric method to select the parameter type to both alphabet and numeric
- */
-Route::get('/parameter-type-alpha-numeric/{string}', function (string $string) {
-  return $string;
-})->whereAlphaNumeric('string');
-
-
-/** 
- * TODO: use the whereIn method to select the parameter from the array return value
- */
-Route::get('/parameter-type-from-array/{array}', function (string $string) {
-  if ($string === 'name' || $string === 'email') {
-    return 'EliteDev Emon';
-  }
-})->whereIn('array', ['post', 'name', 'email']);
-
-/** 
- * TODO: use the where method to select the parameter from the regular expression
- */
-Route::get('regex/{regex}', function (string $string) {
-  return $string;
-})->where('regex', '[0-9a-zA-Z]+@gmail.com');
-
-/**
- * Redirect URL
- * TODO: use redirect method to redirect any url to another url
- */
-Route::redirect('parameter-type-from-regex/{regex}', 'regex/{regex}', 301);
-
-/**
- * Route group with prefix
- */
-Route::prefix('post')->group(function () {
-  Route::get('about', function () {
-    return "This is about route";
-  });
-  Route::get('contact', function () {
-    return "This is contact route";
-  });
+Route::get('check', function(){
+  return "checking successfully";
 });
 
-/**
- * Fallback route
- */
-Route::fallback(function () {
-  return "<h1>Page not found.</h1>";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-/**
- * Blade template syntax
- */
-Route::view('blade-syntax', 'blade-syntax');
-
-/**
- * Make a controller group
- */
-Route::controller(PostController::class)->group(function () {
-  Route::get('index',  'index')->name('index');
-  Route::get('post', 'post')->name('post');
-  Route::get('write', 'write')->name('write');
-});
-
-/**
- * section checking system
- */
-Route::view('section-checking', 'layout-check');
-
-/**
- * *include directive check
- */
-Route::view('include-directive', 'include-directive.body');
-
-/**
- * Send value to view using @param Array method
- * Send value using @param with method
- * Send value with @param keyname in method
- */
-Route::get('post', function () {
-  $name = 'MD EMON HASSAN';
-  // return view('send-value', ['name' => $name]);
-  // return view('send-value')->with('nickname', $name);
-  // return view('send-value')->withName($name);
-});
-
-/**
- * Checking the abort_if() method
- */
 
 
-function getName($index)
-{
-  //variable names
-  $names = [
-    1 => ['name' => 'MD EMON HASSAN'],
-    2 => ['name' => 'ELITEDEV EMON']
-  ];
+require __DIR__.'/auth.php';
 
-  abort_if(!isset($names[$index]), 404);
+Auth::routes();
 
-  // returning the name value
-  return $names[$index]['name'];
-}
-
-Route::get('name/{index}', function ($index) {
-  $name = getName($index);
-  return $name;
-});
-
-/**
- * invokable controller testing
- */
-Route::get('invokable-controller', InvokeController::class);
-
-/**
- * Multiple where condition check
- */
-Route::get('multiple-where-controller', MultipleWhereController::class);
-
-/**
- * Checking pagination links
- */
-Route::get('pagination', PaginationController::class);
-
-/**
- * check group and join status
- */
-Route::get('join-group', GroupController::class);
-
-/**
- * check union for students and teachers table
- */
-Route::get('union-data', TeacherController::class);
-
-/**
- * Form validation with @param request instance
- */
-Route::view('form-validation', 'forms.formValidation');
-Route::post('form-validation', FormValidationController::class)->name('form-validation');
-
-/**
- * Relationship controller
- */
-
-Route::prefix('relation')->group(function (): void {
-  # hasOne
-  Route::get('has-one', hasOneController::class);
-  # hasMany
-  Route::get('has-many', hasManyController::class);
-  # belongsToMany
-  Route::get('belongs-to-many', belongsToManyController::class);
-  #hasOneThrough
-  Route::get('has-one-through', hasOneThroughController::class);
-  #morphTo & morphOne 
-  Route::get('morph-to', morphToController::class);
-});
-
-/**
- * Relationship resource controller
- */
-Route::prefix('relation-resource')->group(function (): void {
-  #morphTo & morphOne
-  Route::resource('morph-to', morphToResourceController::class);
-});
-
-/**
- * Model events and observers route
- */
-Route::prefix('events-observers')->group(function () {
-  #model events
-  Route::get('model-events', modelEventControl::class);
-});
-
-/**
- * create the custom component system
- */
-Route::get('view-component', [checkComponentController::class, 'index'])->name('view-component');
-
-/**
- * check tailwindcss
- */
-Route::view('tailwindcss', 'Tailwindcss.tailwindcss');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
